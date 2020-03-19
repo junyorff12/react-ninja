e2ac118e2a9bf9ff290b8799616ad64eaf6fe024
junyorff12 <junyorff12@gmail.com>
2020-03-19 17:33:32 -0300

    babel config

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,1 +1,9 @@
+import React from 'react';
 
+const Title = React.createClass({
+  render: function () {
+    return React.createElement('h1', null, 'titulo');
+  }
+})
+
+export default Title;
--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,1 +1,8 @@
-console.log('tudo ok.')
+import Title from './app';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
