14dc81740f7f458c2c4a37f4aafa70de485972c1
junyorff12 <junyorff12@gmail.com>
2021-02-17 14:40:51 -0300

    reset project

--- react-webpack/src/button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = ({ children, handleClick }) => (
-  <button className='mainButton' onClick={handleClick}>{children}</button>
-);
-
-export default Button;
--- react-webpack/src/like-button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './button';
-
-const LikeButton = () => (
-  <Button handleClick={() => alert('like')}>Curtir</Button>
-);
-
-export default LikeButton;
